7edd04cb7f3b02d388d0b9dd4496026afcf26b38
Juarez Roberto <juarez.unknown@gmail.com>
2022-07-29 13:17:39 -0500

    :tada: initial commit

---
+++ app/src/main/java/com/juarez/marvelheroes/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.juarez.marvelheroes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // public
+        //eaf2572b49ec3b56f6e2f6e756bf49a8
+        // private
+        // 540d0b54f804a12ea894a2d6278ce881f20edaa8
+    }
+}
